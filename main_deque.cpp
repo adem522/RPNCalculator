3c3892a5830461c0ab2ae3d806375355215fd4e2
Fahri YARDIMCI <ffahri@users.noreply.github.com>
2016-12-04 21:16:44 +0300

    Update main_deque.cpp

--- main_deque.cpp
+++ main_deque.cpp
@@ -1,3 +1,22 @@
+/*
+        _ _       ____  __      _          _                                                         
+       (_) |     / / _|/ _|    | |        (_)                                                        
+   __ _ _| |_   / / |_| |_ __ _| |__  _ __ _                                                         
+  / _` | | __| / /|  _|  _/ _` | '_ \| '__| |                                                        
+ | (_| | | |_ / / | | | || (_| | | | | |  | |                                                        
+  \__, |_|\__/_/  |_| |_| \__,_|_| |_|_|  |_|                                                        
+   __/ |                                                                                             
+  |___/        _ _   _              ________    _          ___     __           _ _                _ 
+ | |          (_) | | |            / /  ____|  | |        (_) \   / /          | (_)              (_)
+ | |___      ___| |_| |_ ___ _ __ / /| |__ __ _| |__  _ __ _ \ \_/ /_ _ _ __ __| |_ _ __ ___   ___ _ 
+ | __\ \ /\ / / | __| __/ _ \ '__/ / |  __/ _` | '_ \| '__| | \   / _` | '__/ _` | | '_ ` _ \ / __| |
+ | |_ \ V  V /| | |_| ||  __/ | / /  | | | (_| | | | | |  | |  | | (_| | | | (_| | | | | | | | (__| |
+  \__| \_/\_/ |_|\__|\__\___|_|/_/   |_|  \__,_|_| |_|_|  |_|  |_|\__,_|_|  \__,_|_|_| |_| |_|\___|_|
+                                                                                                     
+                                                                                                     
+												     
+NOTE : IF YOU CANT COMPILE IN DEVC++ YOU HAVE TO ADD -std=c++11 on compiler options												   
+*/
 #include <iostream>
 #include <stack>
 #include <deque>
@@ -24,11 +43,11 @@ int main()
 	cin>>infix;
 	postfixyap(infix);
 	islemyap();
-	cout<<"Sonuç = "<<kuyruk.front()<<endl;
+	cout<<"SONUC = "<<kuyruk.front()<<endl;
 }
 void islemtablosu(void)
 {
-	cout<<"İŞLEM TABLOSU\n"
+	cout<<"ISLEM TABLOSU"<<endl
 		<<"MATEMATIK OPERATORLERI "<<endl
 		<<"CIKARMA (-)\tTOPLAMA(+)\tCARPMA(*)\tBOLME(/)"<<endl
 		<<"US AL (^)\tMOD ALMA (%)\tNEGATIF (~)\tFAKTORIYEL (!)"<<endl
